4a66f0b25e1361fea1f5c1304a25c104c160544b
Swaroop <swaroopanand325@gmail.com>
2022-07-19 17:21:46 +0800

    added stub data

---
+++ backend/__data__/source.js
@@ -0,0 +1,7 @@
+export const sources = [
+    { id: "01", name: "SRC01" }, 
+    { id: "02", name: "SRC02" }, 
+    { id: "03", name: "SRC03" }, 
+    { id: "04", name: "SRC04" }, 
+    { id: "05", name: "SRC05" }
+]
---
+++ backend/__data__/tickers.js
@@ -0,0 +1,43 @@
+export const tickers = {
+    "SRC01":
+        [
+            { id: "T11", name: "SRC01-T01" },
+            { id: "T12", name: "SRC01-T02" },
+            { id: "T13", name: "SRC01-T03" },
+            { id: "T14", name: "SRC01-T04" },
+            { id: "T15", name: "SRC01-T05" }
+        ],
+    "SRC02":
+        [
+            { id: "T21", name: "SRC02-T01" },
+            { id: "T22", name: "SRC02-T02" },
+            { id: "T23", name: "SRC02-T03" },
+            { id: "T24", name: "SRC02-T04" },
+            { id: "T25", name: "SRC02-T05" }
+        ],
+    "SRC03":
+        [
+            { id: "T31", name: "SRC03-T01" },
+            { id: "T32", name: "SRC03-T02" },
+            { id: "T33", name: "SRC03-T03" },
+            { id: "T34", name: "SRC03-T04" },
+            { id: "T35", name: "SRC03-T05" }
+        ],
+    "SRC04":
+        [
+            { id: "T41", name: "SRC04-T01" },
+            { id: "T42", name: "SRC04-T02" },
+            { id: "T43", name: "SRC04-T03" },
+            { id: "T44", name: "SRC04-T04" },
+            { id: "T45", name: "SRC04-T05" }
+        ],
+    "SRC05":
+        [
+            { id: "T51", name: "SRC05-T01" },
+            { id: "T52", name: "SRC05-T02" },
+            { id: "T53", name: "SRC05-T03" },
+            { id: "T54", name: "SRC05-T04" },
+            { id: "T55", name: "SRC05-T05" }
+        ],
+
+}
