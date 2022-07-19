167fdd2322bc6e142ab8d596571c02e46e0f742e
Swaroop <swaroopanand325@gmail.com>
2022-07-19 18:16:52 +0800

    added utility functions

---
+++ backend/utils/utils.js
@@ -0,0 +1,27 @@
+function generateStockPrice() {
+    let stockprice = Math.random() * 1000;
+    return Number(stockprice.toFixed(2));
+}
+
+function generateTimeStamp(i = 1) {
+    let timeStamp = new Date(Date.now() - i * 5000).toISOString().replace(/\.[0-9]{3}Z/, '').replace('T', ' ');
+    return timeStamp
+}
+
+function generateLastFivePrices() {
+    let lastFivePrices = []
+    for (let i = 0; i < 5; i++) {
+        let priceUnit = {
+            timeStamp: generateTimeStamp(i),
+            stockprice: generateStockPrice()
+        }
+        lastFivePrices.push(priceUnit)
+    }
+    return lastFivePrices
+}
+
+module.exports = {
+    generateLastFivePrices,
+    generateStockPrice,
+    generateTimeStamp
+}
---
+++ backend/utils/utils.test.js
@@ -0,0 +1,29 @@
+const { generateStockPrice, generateTimeStamp, generateLastFivePrices } = require("./utils")
+
+describe("testing utility functions", () => {
+    it("testing generateStockPrice function", () => {
+        jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789);
+        let stockPrice = generateStockPrice()
+        expect(stockPrice).toBe(123.46);
+    })
+
+    it("testing generateTimeStamp function", () => {
+        Date.now = jest.fn(() => 1658225452018);
+        let timeStamp = generateTimeStamp();
+        expect(timeStamp).toBe("2022-07-19 10:10:47");
+    })
+
+    it("testing generateLastFivePrices function", () => {
+        Date.now = jest.fn(() => 1658225452018);
+        let fivePrices = generateLastFivePrices();
+        expect(fivePrices).toStrictEqual(
+            [
+                { "stockprice": 123.46, "timeStamp": "2022-07-19 10:10:52" },
+                { "stockprice": 123.46, "timeStamp": "2022-07-19 10:10:47" },
+                { "stockprice": 123.46, "timeStamp": "2022-07-19 10:10:42" },
+                { "stockprice": 123.46, "timeStamp": "2022-07-19 10:10:37" },
+                { "stockprice": 123.46, "timeStamp": "2022-07-19 10:10:32" }
+            ]
+        );
+    })
+})
